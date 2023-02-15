ebf4561f05baabaf82a0cd6aff384ffbee243c7b
RoPallas <rocio.orquestas@gmail.com>
2023-02-15 08:39:20 -0800

    "4 4 4"

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-/*funtion puts*/
+/**
+ * main - function puts
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+
 	char *str = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return (0);
