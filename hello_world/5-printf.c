61d6886e53366147d62c43746175352d7e0151ad
RoPallas <rocio.orquestas@gmail.com>
2023-02-15 08:57:39 -0800

    "5"

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - function printf
+ *
+ * Return - Always 0 (Sucess)
+ */
+int main(avoid)
+{
+	char *str = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", str);
+	return (0);
+
+}
