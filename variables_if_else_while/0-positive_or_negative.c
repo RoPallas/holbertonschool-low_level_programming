26b1a1c3b41e089612fdba48a128879281a6f973
RoPallas <rocio.orquestas@gmail.com>
2023-02-16 05:06:23 -0800

    "0, without whitespaces"

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- *main - Entry point 
+ *main - Entry point
  *
  *Return: Always 0 (Succecss)
  *
@@ -16,10 +16,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
 		printf("%d is zero\n", n);
-	else if (n > 0) 
+	else if (n > 0)
 		printf("%d is positive\n", n);
-	else 
+	else
 		printf("%d is negative\n", n);
-	
+
 	return (0);
 }
