cd5aa5bcc9fde6e197963fee816ce8933bb944c8
RoPallas <rocio.orquestas@gmail.com>
2023-02-16 08:39:47 -0800

    "Hexadecimal"

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char l;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n + '0');
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
