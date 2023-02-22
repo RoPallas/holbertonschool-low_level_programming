854c8696bc4690b1a7a973b778a85d164033eb8d
RoPallas <rocio.orquestas@gmail.com>
2023-02-22 11:41:53 -0800

    "Print number"

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_number - print an interger
+ * @n: the interger to print
+ */
+void print_number(int n)
+{
+	int d = 1, f = 1, num = n;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num *= -1;
+	}
+	while (f == 1)
+	{
+		if (num / (d * 10) > 0)
+			d *= 10;
+		else
+			f = 0;
+	}
+	while (f == 0)
+	{
+		if (d == 1)
+		{
+			_putchar(num % 10 + '0');
+			f = 1;
+		}
+		else
+		{
+			_putchar((num / d) % 10 + '0');
+			d /= 10;
+		}
+	}
+	_putchar('\n');
+}
