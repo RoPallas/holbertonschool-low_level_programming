92d575de32ae5c5927a74593dd2f4b7ac0d5f1f8
RoPallas <rocio.orquestas@gmail.com>
2023-02-22 08:43:36 -0800

    "Multiplication"

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two interger
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the result of multiplication
+ */
+int mul(int a, int b)
+{
+	int r = a * b;
+
+	return (r);
+}
