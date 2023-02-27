0293f64081a573152710b2ab1a4db32a15a0dcad
RoPallas <rocio.orquestas@gmail.com>
2023-02-27 09:16:51 -0800

    "Fix Save Reverse"

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@
  */
 void rev_string(char *s)
 {
-	int i = 0, strlen = 0, c;
+	int i = 0, strlen = 0, c, n = 0;
 	char a;
 
 	while (s[i] != '\0')
@@ -17,12 +17,13 @@ void rev_string(char *s)
 	}
 	c = strlen;
 	i = 0;
-	while (c >= strlen / 2 && i <= strlen / 2)
+	while (n < strlen / 2)
 	{
 		a = s[i];
 		s[i] = s[c - 1];
-		s[c] = a;
+		s[c - 1] = a;
 		c--;
 		i++;
+		n++;
 	}
 }
