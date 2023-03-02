f9235cb9fde62edccd3380e44c817b0baf85140f
RoPallas <rocio.orquestas@gmail.com>
2023-03-02 05:55:28 -0800

    "Print number"

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_number - print an interger
+ * @n: the interger to print
+ */
+void print_number(int n)
+{
+	int c = 1;
+	unsigned int num, d = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = (unsigned int)(n * -1);
+	}
+	else
+	{
+		num = (unsigned int)n;
+	}
+	while (c == 1)
+	{
+		if (num / (d * 10) >= 10)
+			d *= 10;
+		else
+			c = 0;
+	}
+	while (c == 0)
+	{
+		if (d == 1)
+		{
+			if (num >= 10)
+				_putchar((num / (d * 10)) % 10 + '0');
+			_putchar(num % 10 + '0');
+			c = 1;
+		}
+		else
+		{
+			_putchar((num / (d * 10)) % 10 + '0');
+			d /= 10;
+		}
+	}
+}
