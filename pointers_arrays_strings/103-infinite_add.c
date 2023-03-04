8ee71b5c6ace82df67eed2ea59d8585141e78afe
RoPallas <rocio.orquestas@gmail.com>
2023-03-03 18:02:55 -0800

    "Infinite add"

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -13,38 +13,41 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int len_n1 = 0, len_n2 = 0, i, j;
-	int h = size_r - 2, aux = 0, sum = 0, exit = 0;
+	int h = 0, len_r, l, d = 0, sum = 0;
+	char aux;
 
 	while (n1[len_n1] != '\0')
 		len_n1++;
 	while (n2[len_n2] != '\0')
 		len_n2++;
+	if (len_n1 + 1 >= size_r || len_n2 + 1 >= size_r)
+		return (0);
 	i = len_n1 - 1;
 	j = len_n2 - 1;
-	while (i >= 0i)
+	while (i >= 0 || j >= 0 || d)
 	{
+		sum = d;
+		if (i >= 0)
+		{
+			sum += n1[i] - '0';
+			i--;
+		}
 		if (j >= 0)
-			sum = (n1[i] - '0') + (n2[j] - '0') + aux;
-		else
-			sum = (n1[i] - '0') + aux;
-		if (h >= 0)
 		{
-			r[h] = (sum % 10) + '0';
-			aux = sum / 10;
-			h--;
+			sum += n2[j] - '0';
+			j--;
 		}
-		else
-		{
-			exit = 1;
-			break;
-		}
-		if (i == 0)
-			r[h] = aux + '0';
-		i--;
-		j--;
+		d = sum / 10;
+		r[h] = sum % 10 + '0';
+		h++;
 	}
-	if (exit == 0)
-		return (r);
-	else
-		return(0);
+	r[h] = '\0';
+	len_r = h - 1;
+	for (l = 0; l < len_r / 2; l++)
+	{
+		aux = r[l];
+		r[l] = r[len_r - l];
+		r[len_r - l] = aux;
+	}
+	return (r);
 }
