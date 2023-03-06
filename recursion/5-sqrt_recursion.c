736371b6500d87c58c933bafcb592fdfa6004d22
RoPallas <rocio.orquestas@gmail.com>
2023-03-06 09:24:15 -0800

    "Square root"

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * root - find natural square root of n
+ * @n - number
+ * @i - number test
+ *
+ * Retur: square root
+ */
+int root(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i < n)
+		return (root(n, i + 1));
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ *
+ * Return: the natural square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (root(n, 0));
+}
