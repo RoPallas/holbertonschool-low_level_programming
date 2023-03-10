386cb5508a49c416f6a1aa4f4533da0cbc5d4ea2
RoPallas <rocio.orquestas@gmail.com>
2023-03-10 08:11:47 -0800

    "mul"

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - multiplies two numbers
+ * @argc: count
+ * @argv: strings
+ *
+ * Return: 0 for succes or 1 for error
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc > 1)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
