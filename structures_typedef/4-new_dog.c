7599a2f09154c20bd64fe719aeae85ccf2493718
RoPallas <rocio.orquestas@gmail.com>
2023-03-20 12:10:19 -0700

    "New dog"

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,12 +13,28 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	int namelen = 0, ownerlen = 0, i;
 
+	while (name[namelen])
+		namelen++;
+	while (owner[ownerlen])
+		ownerlen++;
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
-	new_dog->name = name;
+	}
+	new_dog->name = malloc(namelen * sizeof(new_dog->name));
+	if (new_dog == NULL)
+		return (NULL);
+	for (i = 0; i < namelen; i++)
+		new_dog->name[i] = name[i];
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = malloc(ownerlen * sizeof(new_dog->owner));
+	if (new_dog == NULL)
+		return (NULL);
+	for (i = 0; i < ownerlen; i++)
+		new_dog->owner[i] = owner[i];
 	return (new_dog);
 }
