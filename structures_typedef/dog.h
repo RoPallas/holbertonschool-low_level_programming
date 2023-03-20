af558409c837f21fb4cb7e8bfe0d116346839b0e
RoPallas <rocio.orquestas@gmail.com>
2023-03-20 10:47:51 -0700

    "Poppy"

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data of dogs
+ * @name: of dog
+ * @age: of dog
+ * @owner: name of the dog's owner
+ *
+ * Description: struct to save data about dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
