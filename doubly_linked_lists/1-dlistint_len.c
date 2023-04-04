4d6498bf56d18b23be6649c3235fff64ea300aba
RoPallas <rocio.orquestas@gmail.com>
2023-04-04 06:34:52 -0700

    "Len"

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -4,6 +4,8 @@
  * dlistint_len - returns the number of elements in a linked list
  * @h: head of a dlistint_t
  *
+ * Return: the number of elements
+ *
  */
 size_t dlistint_len(const dlistint_t *h)
 {
