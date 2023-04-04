246101bbe604feb743ec06e81eb084e3f460c588
RoPallas <rocio.orquestas@gmail.com>
2023-04-04 08:10:28 -0700

    "Fixing add functions"

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,16 +9,22 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node, *current_node;
+	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node->next = *head;
 	new_node->prev = NULL;
-	current_node = *head;
-	current_node->prev = new_node;
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+	}
 	*head = new_node;
 	return (new_node);
 }
