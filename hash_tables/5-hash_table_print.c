20f5f92e6a01f5c2c40c7aef65262c31cbc7cdd9
RoPallas <rocio.orquestas@gmail.com>
2023-04-16 11:43:54 -0700

    "Print hash table"

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current_node;
+	unsigned long int size = ht->size, i;
+	int p = 0;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < size; i++)
+	{
+		current_node = ht->array[i];
+		while (current_node)
+		{
+			if (p)
+				printf(", ");
+			printf("'%s': '%s'", current_node->key, current_node->value);
+			p = 1;
+			current_node = current_node->next;
+		}
+	}
+	printf("}\n");
+}
